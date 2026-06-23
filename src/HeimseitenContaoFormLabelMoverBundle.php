6154fbac43e86c1e24fa9966c80946f93c0316a0
Niels <info@heimseiten.de>
2026-06-23 10:32:20 +0200

    Modernize bundle layout (src/Resources → project root, AbstractBundle)

--- src/HeimseitenContaoFormLabelMoverBundle.php
+++ src/HeimseitenContaoFormLabelMoverBundle.php
@@ -2,32 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of form label mover.
- * 
- * (c) heimseiten.de - Webdesign aus Köln 2022 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-form-label-mover-bundle
- */
-
 namespace Heimseiten\ContaoFormLabelMoverBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-/**
- * Class HeimseitenContaoFormLabelMoverBundle
- */
-class HeimseitenContaoFormLabelMoverBundle extends Bundle
+class HeimseitenContaoFormLabelMoverBundle extends AbstractBundle
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function build(ContainerBuilder $container): void
-	{
-		parent::build($container);
-		
-	}
 }
